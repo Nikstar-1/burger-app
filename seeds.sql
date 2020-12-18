5559094722daa69ab1669e8f49372735221cfb30
Nikstar-1 <rk_787@hotmail.co.uk>
2020-12-18 17:23:54 +0000

    first commit

---
+++ seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Zinger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Veggie Bean Burger', true);
